c8dea719ec3767d22c1afc3952563dcfd4dedaa6
jjv432 <jackvranicar43@gmail.com>
2024-10-16 08:46:55 -0400

    Updated plotting

--- SlipCalcBoom.m
+++ SlipCalcBoom.m
@@ -1,4 +1,3 @@
-
 clc
 close all
 clearvars
@@ -6,7 +5,7 @@ format compact
 t = readstruct("BoomData_HandActuated.json");
 %{
 
-    Get height to return for the boom!
+    World frame: Z up, x-y in-plane with ground
     
 %}
 
@@ -15,7 +14,7 @@ x = 1;
 z = 1;
 h = 0.5;
 
-%%
+%% Sorting Data
 height = t.height;
 thetas = t.orientation;
 time = t.time;
@@ -25,14 +24,37 @@ z = .01* sin(1:length(thetas));
 
 phis = height * (2*pi)/(4*4096*3);
 
+%% Creating Objects for each part of the system
+Platform.OuterDiameter = 1.5;
+Platform.InnerDiameter = 1.0;
+Platform.Thickness = 0.001; %height between bottom of boom to top of platform
+
+Boom.VerticalDisplacement = 0.05; % meters 
+Boom.Length = 1.25; % m
+Boom.Diameter = .025; % m
+Boom.Phi = phis;
+Boom.Theta = thetas;
+
+Hip.Height = 1; 
+Hip.Width = 0.5;
+Hip.Thickness = 0.5;
+
+Linkage.EndEffector = x; % FIX!
+
+boomPlotter(Platform, Boom, Hip, Linkage);
+
+
+%% Plotting
 for i = 1:length(thetas)
 
 phi = phis(i);
 theta = thetas(i);
 
-nRb = [cos(phi)*cos(theta) - sin(phi)*sin(theta), - cos(phi)*sin(theta) - cos(theta)*sin(phi), 0;
-cos(phi)*sin(theta) + cos(theta)*sin(phi),   cos(phi)*cos(theta) - sin(phi)*sin(theta), 0;
-                                      0,                                           0, 1];
+nRb = [
+    cos(phi)*cos(theta) - sin(phi)*sin(theta), - cos(phi)*sin(theta) - cos(theta)*sin(phi), 0;
+    cos(phi)*sin(theta) + cos(theta)*sin(phi),   cos(phi)*cos(theta) - sin(phi)*sin(theta), 0;
+    0, 0, 1
+    ];
 
 
 bx_hat =  nRb(:,1);
@@ -42,17 +64,19 @@ no_r_fo(:, i) = (L + x(i))* nRb(:, 1) + z(i) * nRb(:,3) + h*[0; 0; 1];
 end
 %%
 % figure()
-% for j = 1:i
-%     hold on
-% plot3(no_r_fo(1,j), no_r_fo(2,j), no_r_fo(3,j), '.')
-% pause(.1)
-% axis([0 1 0 1 0 1])
-% end
-
-figure()
 for j = 1:i
     hold on
-plot(no_r_fo(1,j), no_r_fo(3,j), 'xr')
+plot3(no_r_fo(1,j), no_r_fo(2,j), no_r_fo(3,j), '.')
 pause(.1)
-axis([0 2.5 0.25 .75])
+axis([-2.5 2.5 -2.5  2.5 0 1])
+view(-45, 15);
 end
+
+% figure()
+% for j = 1:i
+%     hold on
+% plot(no_r_fo(2,j), no_r_fo(1,j), '.')
+% pause(.1)
+% %axis([0 2.5 0.25 .75])
+% axis('auto')
+% end
--- boomPlotter.m
+++ boomPlotter.m
@@ -9,17 +9,64 @@ PlatformInnerY = Platform.InnerDiameter * sin(plat_angles);
 PlatformOuterX = Platform.OuterDiameter * cos(plat_angles);
 PlatformOuterY = Platform.OuterDiameter * sin(plat_angles);
 
-Xs = [PlatformInnerX, PlatformOuterX]
-Ys = [PlatformInnerY, PlatformOuterY];
+Xs = [PlatformInnerX, PlatformOuterX, PlatformOuterX, PlatformInnerX]';
+Ys = [PlatformInnerY, PlatformOuterY, PlatformOuterY, PlatformInnerY]';
 
-Xs = [Xs, PlatformOuterX, PlatformInnerX]
-Ys = [Ys,  PlatformOuterY, PlatformInnerY]
+LowZ = zeros(numel(plat_angles), 1);
+HighZ = .5 * ones(numel(plat_angles), 1);
 
-Zs = [zeros(length(plat_angles), 1); zeros(length(plat_angles), 1); Platform.Thickness* ones(length(plat_angles),1); Platform.Thickness* ones(length(plat_angles), 1)]
-
-
+Zs = [LowZ; LowZ; HighZ; HighZ];
 
 figure();
-fill3(Xs, Ys, Zs, 'k')
+Platform.AlphaShape = alphaShape(Xs, Ys, Zs);
+plot(Platform.AlphaShape, 'FaceColor', 'black')
+
+%% Boom Generation
+
+%{
+    !!!!!!!    
+    Need to put the actual transforms in here, these are an approximation
+    for now. This is assuming Z is relatively small. Theres definitely a
+    sin or cos wrong somewhere too
+%}
+
+drawTheta = linspace(0,2*pi, length(Boom.Theta));
+R = Boom.Diameter/2;
+CircleX = R*cos(drawTheta);
+CircleY = R*sin(drawTheta);
+
+figure();
+for i = 1:5:length(Boom.Theta)
+
+    BoomEndX = Boom.Length.*cos(Boom.Theta(i));
+    BoomEndY = Boom.Length.*sin(Boom.Theta(i));
+    BoomEndZ = Boom.Length.*sin(Boom.Phi(i)) + Boom.VerticalDisplacement;
+
+    RealCircleX = CircleX .* cos(Boom.Theta(i));
+    RealCircleY = CircleY .* sin(Boom.Theta(i));
+
+    BeginningX = RealCircleX;
+    BeginningY = RealCircleY;
+
+    EndX = RealCircleX + BoomEndX;
+    EndY = RealCircleY + BoomEndY;
+
+    % Right?
+    CircleZ = R*sin(drawTheta).*cos(Boom.Phi(i)) + Boom.VerticalDisplacement;
+
+    BeginningZ = CircleZ;
+    EndZ = CircleZ + BoomEndZ;
+    Xs = [BeginningX'; EndX'];
+    Ys = [BeginningY'; EndY'];
+    Zs = [BeginningZ'; EndZ'];
+
+    tempAlphaShape = alphaShape(Xs, Ys, Zs);
+    plot(tempAlphaShape, 'FaceColor', 'red')
+    axis([0 2.5 0 2.5 0 1])
+    pause(.01);
+
+
+
+    %clf
 
 end
